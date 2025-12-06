da242fb71ba349a112b38fe7bd4fb7e5643a3cc6
Sunsatt <nika.sanak6@gmail.com>
2025-12-07 00:37:50 +0400

    Change default port from 10000 to 5000

--- app.py
+++ app.py
@@ -140,5 +140,5 @@ def webhook():
     return jsonify({"status": "ok"}), 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))  # Render задаёт PORT
+    port = int(os.environ.get("PORT", 5000))  # Render задаёт PORT
     app.run(host="0.0.0.0", port=port)
